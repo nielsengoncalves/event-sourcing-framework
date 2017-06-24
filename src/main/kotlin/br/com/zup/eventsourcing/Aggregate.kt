06cde66a32b3733549b90f43e90e37d2158d55fe
Luiz Signorelli <luiz.sg@gmail.com>
2017-06-24 19:26:52 +0000

    make AggregateId an open class

--- src/main/kotlin/br/com/zup/eventsourcing/Aggregate.kt
+++ src/main/kotlin/br/com/zup/eventsourcing/Aggregate.kt
@@ -21,5 +21,20 @@ abstract class Aggregate {
 }
 
 data class AggregateVersion(val value: Int)
-data class AggregateId(val value: String)
+open class AggregateId(val value: String) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        other as AggregateId
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+}
 
