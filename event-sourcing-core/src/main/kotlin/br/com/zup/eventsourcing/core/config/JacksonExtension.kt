e5d77ef26da419fa99e40c9735593d42d35a770a
Luiz Signorelli <luiz.sg@gmail.com>
2017-07-04 22:09:48 +0000

    removed no-args module, added jackson-kotlin-module

--- event-sourcing-core/src/main/kotlin/br/com/zup/eventsourcing/core/config/JacksonExtension.kt
+++ event-sourcing-core/src/main/kotlin/br/com/zup/eventsourcing/core/config/JacksonExtension.kt
@@ -3,16 +3,18 @@ package br.com.zup.eventsourcing.core.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 private object JacksonExtension {
 
     val jacksonObjectMapper: ObjectMapper by lazy {
-        val mapper = ObjectMapper()
-        mapper.registerModule(JavaTimeModule())
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        ObjectMapper().registerModule(KotlinModule())
+                .registerModule(JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     }
 
 }
+
 
 fun <T> String.jsonToObject(t: Class<T>): T =
         JacksonExtension.jacksonObjectMapper.readValue(this, t)
